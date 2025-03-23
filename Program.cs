388efbaf0bb9807dfd41b2fae6315c20b2f6aa5e
Vladislav Stepanenko <147318988+fedokgeniy@users.noreply.github.com>
2025-03-24 01:05:59 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+
+try
+{
+    Assembly assembly = Assembly.LoadFrom("LibTask2.dll");                 
+
+    Type phoneType = assembly.GetType("LibTask2.Phone");        
+    Type manufacturerType = assembly.GetType("LibTask2.Manufacturer");
+
+    if (phoneType == null || manufacturerType == null)
+    {
+        throw new Exception("Classes are not found in assembly!");
+    }
+
+    MethodInfo createPhoneMethod = phoneType.GetMethod("Create")!;
+    object[] parameters = { 1, "Nokia 3310", "123456789", "mobile" };
+    object phoneInstance = createPhoneMethod.Invoke(null, parameters);
+
+    MethodInfo createManufacturerMethod = manufacturerType.GetMethod("Create")!;
+    object[] manufacturerParams = { "Nokia", "Helsinki, Finland", false };
+    object manufacturerInstance = createManufacturerMethod.Invoke(null, manufacturerParams);
+
+    if (phoneInstance == null || manufacturerInstance == null)
+    {
+        throw new Exception("Creation of instances was unsuccessfull!");
+    }
+    phoneType.GetMethod("PrintObject")?.Invoke(phoneInstance, null);        
+    manufacturerType.GetMethod("PrintObject")?.Invoke(manufacturerInstance, null);   
+}        
+catch (Exception ex)
+{       
+    Console.WriteLine($"Error {ex.Message}");        
+}       
---
+++ Program2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+using System.Reflection;
+using LibTask2;
+
+    Assembly assembly = typeof(Phone).Assembly;        
+
+    Console.WriteLine($"List of classes in the library: {assembly.GetName().Name}:\n");
+   
+    foreach (Type type in assembly.GetTypes())   
+    {    
+        Console.WriteLine($"Class: {type.Name}");        
+        Console.WriteLine("Property:");    
+
+        foreach (PropertyInfo prop in type.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))    
+        {    
+            string accessModifier = prop.GetMethod?.IsPublic == true ? "Public" : "Private";        
+            Console.WriteLine($"  - {accessModifier} property {prop.Name}");    
+        }    
+
+        Console.WriteLine();    
+    }    
